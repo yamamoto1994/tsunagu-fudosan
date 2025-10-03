fbc854a64ec8a1530f1cecc173f7ab9c3e3f18bc
yamamoto1994 <h.yamamoto@stock-sun.com>
2025-10-03 21:20:23 +0900

    auto commit

---
+++ test.php
@@ -0,0 +1,370 @@
+<div class="f_wrap id_box">
+    <table class="contactform">
+        <tbody>
+            <tr>
+                <th class="th">現在ご検討されている内容<span class="required">必須</span></th>
+                <td>[mwform_checkbox name="soudan" children="新築住宅を検討している。,リフォーム・リノベーションを検討している。,土地探しをしている。"
+                    vertically="true"]</td>
+            </tr>
+            <tr>
+                <th class="th">ご希望される資料<span class="required">必須</span></th>
+                <td>[mwform_checkbox name="shiryou"
+                    children="新築住宅資料,お建て替え資料,リフォーム資料(今のお住まいをリフォームしたい方),リフォーム資料(中古住宅を購入してリフォームしたい方)" vertically="true"]
+                </td>
+            </tr>
+            <tr>
+                <th class="th">お名前<span class="required">必須</span></th>
+                <td>[mwform_text name="name"]</td>
+            </tr>
+            <tr>
+                <th class="th">ふりがな</th>
+                <td>[mwform_text name="furigana"]</td>
+            </tr>
+            <tr>
+                <th class="th">メールアドレス<span class="required">必須</span></th>
+                <td>[mwform_text name="email"]</td>
+            </tr>
+            <tr>
+                <th class="th">電話番号<span class="required">必須</span></th>
+                <td class="td_name">[mwform_text name="tel"]</td>
+            </tr>
+            <tr>
+                <th class="th">郵便番号</th>
+                <td>[mwform_text name="zip" id="zip" class="zip"]</td>
+            </tr>
+            <tr>
+                <th class="th">ご住所<span class="required">必須</span></th>
+                <td>[mwform_text name="address" id="address"]<br />
+                    <span style="font-size: 90%;">カタログの発送に使用致します。番地・建物名・号室などまでご記入をお願い致します。</span>
+                </td>
+            </tr>
+            <tr>
+                <th class="th">建築予定地について</th>
+                <td>[mwform_radio name="yotei"
+                    children="現在所有している(自己所有),現在所有している(親族所有),建て替えを検討している,購入予定地が決定している,探している/探しはじめ" vertically="true"]
+                </td>
+            </tr>
+            <tr>
+                <th class="th">建築予定地のエリア</th>
+                <td class="td_name">[mwform_text name="yoteiarea"]</td>
+            </tr>
+            <tr>
+                <th class="th">建物本体のご予算イメージ</th>
+                <td>[mwform_radio name="money"
+                    children="予算検討中・これから検討,1500万円〜2000万円,2000万円〜2500万円,2500万円〜3000万円,3000万円〜3500万円,3500万円以上"
+                    vertically="true"]</td>
+            </tr>
+            <tr>
+                <th class="th">新居へのご入居希望時期について</th>
+                <td>[mwform_radio name="jiki" children="１年以内,１年～２年以内,２年～３年以内,３年以上先,希望時期なし" vertically="true"]</td>
+            </tr>
+            <tr>
+                <th class="th">サンプロ建築設計を知ったきっかけは？</th>
+                <td style="height: 212px;">[mwform_checkbox name="kikkake"
+                    children="新聞折込チラシ,投函チラシ,DM,Instagram,YouTube,facebook,LINE,suumo,雑誌・フリーペーパー,紹介,インターネット検索,バナー広告,イベント,その他"
+                    vertically="true"]</td>
+            </tr>
+            <tr>
+                <th class="th">ご質問・ご要望等</th>
+                <td>[mwform_textarea name="naiyou"]<br />
+                    <span style="font-size: 90%;">事前に確認したいことや、家づくりでこだわりたいポイントなどございましたらぜひご記入ください。</span>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <div class="agree">[mwform_checkbox name="doui" children="上記内容でよろしければ、チェックを入れて下さい" separator=","]<span
+            class="required">必須</span></div>
+    <div class="section">
+        <div class="contat_info">
+            <p class="title">ドメイン指定受信のお願い</p>
+            <p class="txt">sunpro36.co.jp のドメイン指定受信の設定をお願い致します。<br />
+                お申込みして頂いたお客様には、自動で受付確認メールを送信しております。受付確認メールを受信されていないお客様は、受信設定により弊社からのメールを受信拒否している恐れがございますので、ご確認下さい。特にキャリアメールは、キャリアによって複数の受信設定がございますので、事前にご確認お願い致します。
+            </p>
+            <p class="title">施工可能エリア</p>
+            <p class="txt">・北信：長野市、中野市、小布施町、須坂市、千曲市<br />
+                ・東信：上田市、東御市、坂城町、青木村、佐久市、小諸市、立科町、御代田町<br />
+                ・中信：松本市、塩尻市、安曇野市、松川村、池田町、山形村、朝日村、筑北村、生坂村、麻績村<br />
+                ・南信：岡谷市、下諏訪町、諏訪市、茅野市、辰野町、原村、富士見町、伊那市、箕輪町、南箕輪村、宮田村、駒ケ根市、飯島町</p>
+            <p class="txt">※要相談エリア<br />
+                飯綱町、飯山市、山ノ内町、高山村、白馬村、小川村、大町市、長和町<br class="pc" />木祖村、木曽町、松川町、高森町、飯田市</p>
+        </div>
+        <div class="form-consent">
+            <p>このまま進むことで、当社の<a href="https://sunpro36.co.jp/company/privacy/" target="_blank"
+                    rel="noopener">個人情報取扱方針</a>にご同意いただいたものとしてお手続きを承ります。</p>
+            <p>内容をご確認のうえ、よろしければボタンを押してお進みください。</p>
+        </div>
+    </div>
+    <p class="contactBtn">[mwform_submitButton name="mwform_submitButton" confirm_value="確認する" submit_value="送信する"
+        class="submit"]<br />
+        [mwform_backButton value="編集へ戻る" class="backBtn"]</p>
+</div>
+
+<style>
+    @media screen and (max-width: 756px) {
+        .green_ttl.line_none {
+            font-size: 5.5vw;
+            text-align: center;
+            color: #7fbe26;
+            font-weight: 700;
+            font-family: "ヒラギノ角ゴ Pro", "Hiragino Kaku Gothic Pro", "メイリオ", Meiryo, "MS Pゴシック", "MS PGothic", sans-serif;
+            letter-spacing: 2px;
+            margin-bottom: 1em;
+            padding-top: 130px;
+            position: relative;
+        }
+
+        input {
+            padding: 0.8rem !important;
+        }
+
+        input[type=checkbox] {
+            width: 1.8rem !important;
+            height: 1.8rem !important;
+        }
+
+        .contactform tr {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .contactform tr th.th {
+            width: 100% !important;
+            display: block !important;
+            /* 横並び */
+            align-items: center !important;
+            /* 縦位置揃え */
+            gap: .5em !important;
+            /* テキストと必須の間隔 */
+            position: relative !important;
+            padding: 1em 1em 0em 0 !important;
+            text-align: left !important;
+            vertical-align: top !important;
+            width: 100% !important;
+            font-size: 1.4rem!important;
+            font-weight: bold !important;
+            color: black !important;
+
+        }
+
+        .contactform tr th .required {
+            float: none !important;
+            position: static !important;
+            display: inline-block !important;
+            margin: 0 !important;
+            padding: 0.2em 0.8em !important;
+            font-size: 1.4rem !important;
+            background: #FF6060 !important;
+            color: #fff !important;
+            border-radius: 4px !important;
+        }
+
+        .contactform tr td {
+            width: 100% !important;
+        }
+
+        .contactform tr td {
+            padding: 0.5em 0 !important;
+        }
+
+        input[type=radio] {
+            padding: 0.5rem !important;
+            width: 18px !important;
+            height: 18px !important;
+        }
+
+        .contactform tr td {
+            height: auto !important;
+        }
+
+        td>span {
+            line-height: 2;
+
+        }
+
+        .contactform {
+            margin-bottom: 30px !important;
+        }
+
+        .agree {
+            margin-bottom: 30px !important;
+        }
+
+        .agree .required {
+            display: none;
+        }
+
+        input,
+        textarea {
+            width: 100% !important;
+            box-sizing: border-box!important;
+        }
+
+        .required,
+        .optional {
+            font-size: 0.8125rem!important;
+            color: #fff!important;
+            margin-left: 0!important;
+            margin-right: 1em!important;
+            padding: 0 1em!important;
+            display: inline-block!important;
+        }
+
+        .required {
+            background-color: #FF6060!important;
+        }
+    }
+</style>
+
+--------
+
+
+
+<style>
+    @media screen and (max-width: 756px) {
+
+        .green_ttl.line_none {
+            font-size: 5.5vw;
+            text-align: center;
+            color: #7fbe26;
+            font-weight: 700;
+            font-family: "ヒラギノ角ゴ Pro", "Hiragino Kaku Gothic Pro", "メイリオ", Meiryo, "MS Pゴシック", "MS PGothic", sans-serif;
+            letter-spacing: 2px;
+            margin-bottom: 1em;
+            padding-top: 130px;
+            position: relative;
+        }
+
+        input {
+            padding: 0.8rem !important;
+        }
+
+        input[type=checkbox] {
+            width: 1.8rem !important;
+            height: 1.8rem !important;
+        }
+        
+        .form__wrap {
+            padding: 0 5vw!important;
+        }
+
+        .contactform tr {
+            display: flex;
+            flex-direction: column;
+            border-bottom: 1px solid #000;
+            margin-bottom: 2rem;
+            padding-bottom: 2rem;
+        }
+
+        .contactform tr th.th {
+            width: 100% !important;
+            display: block !important;
+            /* 横並び */
+            align-items: center !important;
+            /* 縦位置揃え */
+            gap: .5em !important;
+            /* テキストと必須の間隔 */
+            position: relative !important;
+            padding: 1em 1em 0.4em 0 !important;
+            text-align: left !important;
+            vertical-align: top !important;
+            width: 100% !important;
+            font-size: 1.4rem!important;
+            font-weight: bold !important;
+            color: black !important;
+
+        }
+
+        .contactform tr th .required {
+            float: none !important;
+            position: static !important;
+            display: inline-block !important;
+            margin: 0 !important;
+            padding: 0.2em 0.8em !important;
+            font-size: 1.4rem !important;
+            background: #FF6060 !important;
+            color: #fff !important;
+            border-radius: 4px !important;
+        }
+
+        .contactform tr td {
+            width: 100% !important;
+        }
+
+        .contactform tr td {
+            padding: 0.5em 0 !important;
+        }
+
+        input[type=radio] {
+            padding: 0.5rem !important;
+            width: 18px !important;
+            height: 18px !important;
+        }
+
+        .contactform tr td {
+            height: auto !important;
+        }
+
+        td>span {
+            line-height: 2;
+
+        }
+
+        .contactform {
+            margin-bottom: 30px !important;
+        }
+
+        .agree {
+            margin-bottom: 30px !important;
+        }
+
+        .agree .required {
+            display: none;
+        }
+
+        input,
+        textarea {
+            width: 100% !important;
+            box-sizing: border-box!important;
+        }
+
+        .required,
+        .optional {
+            font-size: 0.8125rem!important;
+            color: #fff!important;
+            margin-left: 0!important;
+            margin-right: 1em!important;
+            padding: 0 1em!important;
+            display: inline-block!important;
+        }
+
+        .required {
+            background-color: #FF6060!important;
+            margin-left: 10px!important;
+        }
+        .contat_info {
+        border: 1px solid #000;
+    padding: 8vw;
+    position: relative;
+    line-height: 2.15;
+    text-align: left;
+    margin-bottom: 20px;
+    
+        }
+        .contat_info .title {
+            font-size: 1.6rem;
+        }
+        .contat_info .txt {
+            font-size: 1.2rem;
+        }
+        
+        .form-consent {
+            padding-bottom: 20px;
+        }
+        .form-consent p {
+            font-size: 1.2rem;
+            line-height: 1.75em;
+        }
+    }
+</style>
+
+<?php get_footer( 'lp2' ); ?>
